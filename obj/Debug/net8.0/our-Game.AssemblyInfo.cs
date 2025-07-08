6c6b32b0b42f9e4d1c99dd96bc8ba9b617224047
Lukas Berlin <lukasberlin.stendal@gmail.com>
2025-07-08 21:12:14 +0200

    +Knight (Knightord)

--- obj/Debug/net8.0/our-Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/our-Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("our-Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edbce4b26d8caa5cc7fa88ecd9102d5a226547bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+081aa764e6b8e8edf013869dd00ffdd0871229ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("our-Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("our-Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
