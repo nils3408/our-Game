80bb61f1e34c7e606e740ede4085ecfe1675d20a
nilsw <nils.winterst001@gmail.com>
2025-09-10 01:17:06 +0200

    robot

--- obj/Release/net8.0/our-Game.AssemblyInfo.cs
+++ obj/Release/net8.0/our-Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("our-Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75de671bcc60a91e0a42b3109bb11e9288fafce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b193ba06841346e8282a8d70472d46b271541c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("our-Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("our-Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/PlayerStuff/Robot.cs
+++ src/PlayerStuff/Robot.cs
@@ -3,16 +3,21 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Math = System.Math;
+using Random = System.Random;
 
 public class Robot : Player
 {
 
     private GameLogic game;
+    Random rnd = new Random();
+
 
     public Robot(GraphicsDevice graphicsDevice, Vector2 position1, Texture2D texture1, Texture2D shoot_texture1, Texture2D special_move_texture1, int player, PlayerControls controls, GameLogic game)
               : base(graphicsDevice, position1, texture1, shoot_texture1, special_move_texture1, player, controls)
     {
         this.game = game;
+        moving_direction = -1;
     }
 
     public override void handle_input(float delta)
@@ -20,28 +25,35 @@ public class Robot : Player
         Vector2 BV = game.getBall().velocity;
         Vector2 BP = game.getBall().position;
         Vector2 RP = base.position;
+        Vector2 OP = otherPlayer.position;
 
         //Debug.WriteLine($"Ball Position: {BP}");
 
-        if (BV.X < 0 && (BP.X+60) < RP.X)
+        if (BV.X < 0 && (BP.X + 60) < RP.X)
         {
 
             moveLeft(delta);
         }
-        else 
+        else
         {
-            if(RP.X < 1600) moveRight(delta);
-            
-            if ( RP.X - BP.X < 10) {
-                shoots_horizontal = true;
+            if (RP.X < 1600) moveRight(delta);
 
-                Debug.WriteLine($"Ball Position: {BP}, Robot Position: {RP} ");
+            if (RP.X - BP.X < 10 && Math.Abs(RP.X - BP.X) < 10)  // Bot is next to the ball and right of the ball
+            {
+                reset();   //reset before shoot, to make sure the right shoot will be executed
+                shoot();
+
+                //Debug.WriteLine($"Ball Position: {BP.X}, Robot Position: {RP.X} ");
             }
         }
 
-        if (BV.X > 0 &&  (BP.Y + 50 * BV.Y) < 300) {
+        if (BV.X > 0 && (BP.Y + 50 * BV.Y) < 300)
+        {
             jump(delta);
         }
+
+
+        activate_PowerUp();
 
     }
 
@@ -52,8 +64,103 @@ public class Robot : Player
         else { move_helper(delta, 1); }
     }
     private void moveRight(float delta)
-    { 
-        if (MoveChange_powerup_in_use == false)     { move_helper(delta, 1); }
-        else                                        { move_helper(delta, -1);  }
+    {
+        if (MoveChange_powerup_in_use == false) { move_helper(delta, 1); }
+        else { move_helper(delta, -1); }
     }
+
+    private void shoot()
+    {
+        int shootType = getShootType();
+
+        switch (shootType)
+        {
+            case 1: { shoots_horizontal = true; break; }
+            case 2: { shoots_lupfer = true; break; }
+            case 3: { shoots_diagonal = true; break; }
+        }
+    }
+
+
+    private int getShootType()
+    {
+        if ((otherPlayer.position.Y + otherPlayer.RectangleHeight) <= game.getBall().position.Y || //player is over the ball
+            otherPlayer.velocity.Y < 0)                                                             //player is jumping
+        {
+            return 1;
+        }
+
+        int value = rnd.Next(2, 4); // 2 inklusiv, 4 exklusiv --> liefert 2 oder 3
+        return value;
+    }
+
+    public void reset()
+    {
+        //reset shoots as id does not have the reset_when_key_is_released() as the other player do
+        shoots_diagonal = false;
+        shoots_horizontal = false;
+        shoots_lupfer = false;
+    }
+
+
+
+    //--------------------------------------------------------
+    // powerups
+    public void activate_PowerUp()
+    {
+        if (powerup1 != null)
+        {
+            switch (powerup1)
+            {
+                case BigPlayerPowerUp _:
+                    {
+                        if (BigPlayerPowerUpActivationMakesSense()) activate_powerUP(1);
+                        break;
+                    }
+
+                case SmallPlayerPowerUp _:
+                    if (SmallPlayerPowerUpActivationMakesSense()) activate_powerUP(1);
+                    break;
+
+                        default:
+                    break;
+            }
+        }
+
+
+        if (powerup2 != null)
+        {
+            switch (powerup2)
+            {
+                case BigPlayerPowerUp _:
+                    if (BigPlayerPowerUpActivationMakesSense()) activate_powerUP(2);
+                    break;
+
+                case SmallPlayerPowerUp _:
+                    if (SmallPlayerPowerUpActivationMakesSense()) activate_powerUP(2);
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+
+
+    public bool BigPlayerPowerUpActivationMakesSense()
+    {
+        // makes sense if not currently activated
+        // size <= normal_size -> he ist not big currently
+        return RectangleHeight <= RectangleHeight_copy;
+    }
+
+    public bool SmallPlayerPowerUpActivationMakesSense()
+    {
+        // makes sense if not currently activated
+        // otherPlayer.size >= normal_size -> he ist not small currently
+        return otherPlayer.RectangleHeight >= RectangleHeight_copy;
+    }
+
+
 }
